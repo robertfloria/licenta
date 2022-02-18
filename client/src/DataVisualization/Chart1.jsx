d99e230970b4f0512f65096dd6e26d0b09328f42
robertfloria <robertfloria27@gmail.com>
2022-02-18 18:58:51 +0200

    transform class component into functional component for d3 setup

--- client/src/DataVisualization/Chart1.jsx
+++ client/src/DataVisualization/Chart1.jsx
@@ -1,48 +1,43 @@
-import React, {Component} from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import * as  d3 from 'd3';
 
-class BarChart extends Component {
-  constructor(props){
-    super(props);
-    this.myRef = React.createRef();
-    
-  }
-  componentDidMount(){
-    this.drowChart();
-  }
+export default function BarChart (props) {
 
-  drowChart() {
-    const data = this.props.data;
-   
-    const svg = d3.select(this.myRef.current)
-                  .append("svg")
-                  .attr("width", this.props.width)
-                  .attr("height", this.props.height);
-                  
-    svg.selectAll("rect")
-       .data(data)
-       .enter()
-       .append("rect")
-       .attr("x", (d, i) => i * 70)
-       .attr("y", (d, i) => this.props.height - 10 * d)
-       .attr("width", 65)
-       .attr("height", (d, i) => d * 10)
-       .attr("fill", "green");
+    const myRef = useRef(null);
 
-    svg.selectAll("text")
-       .data(data)
-       .enter()
-       .append("text")
-       .text((d) => d)
-       .attr("x", (d, i) => i * 70)
-       .attr("y", (d, i) => this.props.height - (10 * d) - 3)
-       
-  };
+    useEffect(() => {
+      
+      drowChart();
+    });
 
+    const drowChart = () => {
 
-  render(){  
-    return <div ref = {this.myRef}></div>
-  }
+      const data = props.data;   
+      const svg = d3.select(myRef.current)
+                    .append("svg")
+                    .attr("width", props.width)
+                    .attr("height", props.height);
+                    
+      svg.selectAll("rect")
+         .data(data)
+         .enter()
+         .append("rect")
+         .attr("x", (d, i) => i * 70)
+         .attr("y", (d, i) => props.height - 10 * d)
+         .attr("width", 65)
+         .attr("height", (d, i) => d * 10)
+         .attr("fill", "green");
+  
+      svg.selectAll("text")
+         .data(data)
+         .enter()
+         .append("text")
+         .text((d) => d)
+         .attr("x", (d, i) => i * 70)
+         .attr("y", (d, i) => props.height - (10 * d) - 3); 
+    };
+
+    return (
+      <div ref = {myRef}></div>
+    )
 }
-
-export default BarChart;
