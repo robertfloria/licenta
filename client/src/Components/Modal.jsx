262bdb8b37498ee7dd6cf768d25594e0d4b44fc9
robertfloria <robertfloria27@gmail.com>
2022-02-22 12:20:41 +0200

    a demo modal has been created

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
-export default function Modal () {
+import "./Modal.css";
+export default function Modal ({closeModal}) {
     return (
-        <div>
-            Modal
+        <div className='modalBackground'>
+            <div className='modalContainer'>
+                <div className='closeButton'>
+                    <button onClick={() => closeModal(false)}>X</button>
+                </div>
+                <div className='title'>
+                    <h1>Continue if you have courage!</h1>
+                </div>
+                <div className='body'>
+                    <p>Move forword</p>
+                </div>
+                <div className='footer'>
+                    <button onClick={() => closeModal(false)} id='cancelButton'>Cancel</button>
+                    <button>Continue</button>
+                </div>             
+            </div>
         </div>
     )
 }
--- client/src/OpenModal.js
+++ client/src/OpenModal.js
@@ -1,12 +1,25 @@
 import Modal from "./Components/Modal";
-import "./Stylesheet/App.scss";
+import "./Components/Modal.css";
+import { useState } from "react";
 
 export default function OpenModal() {
+
+    const [openModal, setOpenModal] = useState(false);
+
     return (
         <div className="openModal">
             <h1>Open Modal</h1>
-            <button className="openModalBtn">Open</button>
-            <Modal />
+
+            <button 
+                className="openModalBtn" 
+                onClick={() => {
+                    setOpenModal(true)
+                }}
+            >
+                Open
+            </button>           
+            
+            {openModal && <Modal closeModal={setOpenModal}/>} {/* if statement, daca openModal este true atunci se executa functia Modal */}
         </div>
     )
 }
