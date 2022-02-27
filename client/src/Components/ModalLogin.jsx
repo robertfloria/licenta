dbe2451d92b8e286ea6ead876afaf8db4a7ec6b5
robertfloria <robertfloria27@gmail.com>
2022-02-27 16:57:59 +0200

    successfully integrated login/register backend server into modals

--- client/src/Components/ModalLogin.jsx
+++ client/src/Components/ModalLogin.jsx
@@ -1,21 +1,80 @@
 import React from 'react';
 import { useState } from "react";
 import "./Modal.css";
+import Axios from "axios";
 
 const LoginModal = (props) => {
   
+    const [userName, setUserName] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [newUserPassword, setNewUserPassword] = useState("");
+    const [newUserEmail, setNewUserEmail] = useState("");
+
+    const getLogin = () => {
+        Axios.post("http://localhost:3001/api/login", {
+          username: userName, 
+          password: userPassword
+        })
+      };
+    
+      const deleteUser = (userName) => {
+        Axios.delete(`http://localhost:3001/api/delete/${userName}`)
+      };
+    
+      const updatePassword = () => {
+        Axios.put("http://localhost:3001/api/update/password", {
+          username: userName,
+          password: newUserPassword
+        });
+        setNewUserPassword("");
+      };
+    
+      const updateEmail = () => {
+        Axios.put("http://localhost:3001/api/update/email", {
+          username: userName,
+          email: newUserEmail
+        });
+        setNewUserEmail("");
+      };
+
     return (
         <>
             <div className='modalLogin'>
                 <div className='title'>
                     <h1>Login</h1>
                 </div>
-                <div className='body'>
-                    <p>Move forword</p>
+                <div className='form'>
+                    <label>Username</label>
+                    <input type="text" name="userName" onChange={(e) => {
+                        setUserName(e.target.value);
+                    }} 
+                    />
+
+                    <label>Password</label>
+                    <input type="password" name="userPassword" onChange={(e) => {
+                        setUserPassword(e.target.value);
+                    }} 
+                    />
+                    
+                    <div>
+                        <input type="text" name="newUserEmail" onChange={(e) => {
+                            setNewUserEmail(e.target.value);
+                          }}
+                        />
+                        <button onClick={updateEmail}>Update Email</button>   
+                        
+                        <input type="password" name="newUserPassword" onChange={(e) => {
+                            setNewUserPassword(e.target.value);
+                          }}
+                        />
+                        <button onClick={updatePassword}>Update Password</button>     
+                        <button onClick={() => {deleteUser(userName)}}>Delete User</button>        
+                    </div>
                 </div>
+
                 <div className='footer'>
                     <button onClick={() => props.closeModal(false)} id='cancelButton'>Cancel</button>
-                    <button>Login</button>
+                    <button onClick={getLogin}>Login</button>
                     <a href='#' onClick={()=>{
                         props.openRegister(true); props.openLogin(false);
                         }}>Go to Register</a>
--- client/src/Components/ModalRegister.jsx
+++ client/src/Components/ModalRegister.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
 import { useState } from "react";
 import "./Modal.css";
+import Axios from "axios";
 
 const RegisterModal = (props) => {
   
+    const [userName, setUserName] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+
+    const insertCredentials = () => {
+      Axios.post("http://localhost:3001/api/insert/credentials", {
+        username: userName, 
+        password: userPassword,
+        email: userEmail
+      });
+    };
+
     return (
         <>               
             <div className='modalRegister'>
                 <div className='title'>
                     <h1>Register</h1>
                 </div>
-                <div className='body'>
-                    <p>Move forword</p>
+                <div className='form'>
+                    <label>Username</label>
+                    <input type="text" name="userName" onChange={(e) => {
+                        setUserName(e.target.value);
+                    }} 
+                    />
+        
+                    <label>Password</label>
+                    <input type="text" name="userPassword" onChange={(e) => {
+                        setUserPassword(e.target.value);
+                    }} 
+                    />
+        
+                    <label>Email</label>
+                    <input type="text" name="userEmail" onChange={(e) => {
+                        setUserEmail(e.target.value);
+                    }} 
+                    />
                 </div>
                 <div className='footer'>
                     <button onClick={() => props.closeModal(false)} id='cancelButton'>Cancel</button>
-                    <button>Register</button>
+                    <button onClick={insertCredentials}>Submit</button>
                     <a href='#' onClick={()=>{
                         props.openRegister(false); props.openLogin(true);
                         }}>Go to Login</a>
                 </div>       
             </div>  
-        </>           
+        </>
     )
 }
 export default RegisterModal;
