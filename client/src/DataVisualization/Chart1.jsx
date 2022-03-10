aee025c43664eec3819a5651bbe17e4535453365
robertfloria <robertfloria27@gmail.com>
2022-03-10 14:54:49 +0200

    setup d3 and clean the code

--- client/src/DataVisualization/Chart1.jsx
+++ client/src/DataVisualization/Chart1.jsx
@@ -41,3 +41,34 @@ export default function BarChart (props) {
       <div ref = {myRef}></div>
     )
 }
+
+
+
+/*
+    const drowChart = () => {
+
+      const data = props.data;   
+      const svg = d3.select(myRef.current)
+                    .append("svg")
+                    .attr("width", props.width)
+                    .attr("height", props.height);
+                    
+      svg.selectAll("rect")
+         .data(data)
+         .enter()
+         .append("rect")
+         .attr("x", (d, i) => i * 70)
+         .attr("y", (d, i) => props.height - 10 * d)
+         .attr("width", 65)
+         .attr("height", (d, i) => d * 10)
+         .attr("fill", "green");
+  
+      svg.selectAll("text")
+         .data(data)
+         .enter()
+         .append("text")
+         .text((d) => d)
+         .attr("x", (d, i) => i * 70)
+         .attr("y", (d, i) => props.height - (10 * d) - 3); 
+    };
+*/
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Register from './Register';
-import Login from './Login';
 import App from './DataVisualization/App';
 import OpenModal from './OpenModal';
 
-ReactDOM.render(<OpenModal />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
