123649493f2b0130037ef92bb294e8b0661fc489
robertfloria <robertfloria27@gmail.com>
2022-04-23 20:48:55 +0300

    sidebar first look have been implemented

--- client/src/Components/ModalMenu.jsx
+++ client/src/Components/ModalMenu.jsx
@@ -1,20 +1,79 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import "../App.css"
+import "../App.css";
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import PieChartIcon from '@mui/icons-material/PieChart';
+import DonutSmallIcon from '@mui/icons-material/DonutSmall';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Sidebar () {
 
+    const [barStatus, setBatStatus] = useState(true);
+    
     const sideBarData = [ 
         {
             title: "Home",
-            //icon: <Icon />,
+            icon: <HomeIcon />,
             link: "/home"
+        },
+        {
+            title: "User",
+            icon: <AccountCircleIcon />,
+            link: "/Account"
+        },
+        {
+            title: "BarChart",
+            icon: <BarChartIcon />,
+            link: "/Barchart"
+        },
+        {
+            title: "PieChart",
+            icon: <PieChartIcon />,
+            link: "/Piechart"
+        },
+        {
+            title: "SsIdChart",
+            icon: <SsidChartIcon />,
+            link: "/Ssidchart"
+        },
+        {
+            title: "DonutChart",
+            icon: <DonutSmallIcon />,
+            link: "/Donutchart"
+        },
+        {
+            title: "Add",
+            icon: <AddIcon />,
+            link: "/Add"
+        },
+        {
+            title: "Logout",
+            icon: <LogoutIcon />,
+            link: "/Logout"
         }
     ];
 
     return (
-        <div className='sideBar'>
-
+        <div className='sideBarBackground'>
+            <div className='sideBar'> 
+                <ul className='sideBarList'>
+                    {sideBarData.map((val, key) => {
+                        return (
+                            <li key={key} className='row' 
+                            id={window.location.pathname == val.link ? "active" : ""} 
+                            onClick={()=> {window.location.pathname = val.link}}
+                            >
+                                <div id='icon'>{val.icon}</div>
+                                <div id='title'>{val.title}</div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
