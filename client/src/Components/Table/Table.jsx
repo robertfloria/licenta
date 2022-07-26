f1266b7a562c2260e02e30d48d7daccd59840eba
robertfloria <robertfloria27@gmail.com>
2022-07-26 17:10:22 +0300

    table have been implemented

--- client/src/Components/Table/Table.jsx
+++ client/src/Components/Table/Table.jsx
@@ -4,6 +4,7 @@ import Axios from "axios";
 import { useTable } from 'react-table';
 import MOCK_DATA from '../../DataVisualization/data/MOCK_DATA.json'
 import { COLUMNS } from './Columns';
+import 'D:/GitHub/licenta/client/src/Components/Table/Table.css';
 
 export const TableGrid = (props) => {
   
@@ -25,33 +26,48 @@ export const TableGrid = (props) => {
     } = tableInstance;
 
     return (
-        <>               
+        <div className='dadadada'>               
             <div className='tableGrid'>
                 <table {...getTableProps}>  
                     <thead>
-                        { headerGroups.map((headerGroup) => {
-                            return (
-                                <tr {...headerGroup.getHeaderGroupProps()}>
-                                    {
-                                        headerGroup.headers.map((column) => {
-                                            return (
-                                                <th {...column.getHeaderProps()}> {column.render('Header')} </th>
-                                            )             
-                                        })
-                                    }                       
-                                </tr>
-                            )
-                        })}
+                        { 
+                            headerGroups.map((headerGroup) => {
+                                return (
+                                    <tr {...headerGroup.getHeaderGroupProps()}>
+                                        {
+                                            headerGroup.headers.map((column) => {
+                                                return (
+                                                    <th {...column.getHeaderProps()}> {column.render('Header')} </th>
+                                                )             
+                                            })
+                                        }                       
+                                    </tr>
+                                )
+                            })
+                        }
 
                     </thead>
                     <tbody {...getTableBodyProps}>
-                        <tr>
-                            <td></td>
-                        </tr>
+                        {
+                            rows.map((row) => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {
+                                            row.cells.map((cell) => {
+                                                return  <td {...cell.getCellProps()}> {cell.render('Cell')} </td>
+                                            })
+                                        }
+
+                                    </tr>
+                                )
+                            })
+                        }
+
                     </tbody>
                 </table>
             </div>  
-        </>
+        </div>
     )
 }
 
